4b3a67b63834ad5a0c13791b5af9e9f00b280c11
maykeasilva <maykesilva1703@gmail.com>
2023-04-25 18:31:02 -0300

    Challenge #01

---
+++ react/challenge-01/README.md
@@ -0,0 +1,8 @@
+## Desenvolva uma aplicação em que:
+
+* O usuário pode clicar em qualquer lugar da página.
+* Deve-se renderizar um pequeno círculo na posição clicada.
+* A cada clique, mantém-se os círculos já criados e renderiza-se um novo.
+* Crie duas funcionalidades para a aplicação:
+  * Desfazer (undo)
+  * Refazer (redo)
--- react/challenge-01/src/App.jsx
+++ react/challenge-01/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => { 
+  const [dots, setDots] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+
+    setDots((prev) => [...prev, newDot]);
+  };
+
+  const handleUndo = (event) => {
+    event.stopPropagation();
+
+    if (dots.length === 0)
+      return;
+
+    const lastDot = dots[dots.length - 1];
+    setUndid((prev) => [...prev, lastDot]);
+
+    setDots((prev) => {
+      const newArray = [...prev].slice(0, -1);
+      return newArray;
+    });
+  };
+
+  const handleRedo = (event) => {
+    event.stopPropagation();
+
+    if (undid.length === 0)
+      return;
+
+    const recoveredDot = undid[undid.length - 1];
+    setUndid((prev) => {
+      const newArray = [...prev].slice(0, -1);
+      return newArray;
+    });
+
+    setDots((prev) => [...prev, recoveredDot]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div id='main' onClick={handleClick}>
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
+      {
+        dots.map((value, index) => {
+          return <span key={index} className='dot' style={{top: value.clientY, left: value.clientX}}></span>
+        })
+      }
+    </div>
+  );
+};
 
-export default App
+export default App;
